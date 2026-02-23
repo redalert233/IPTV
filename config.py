2a2e30db0b0b981f283c3bdd74c82613632da432
redalert233 <49602835+redalert233@users.noreply.github.com>
2026-02-23 20:07:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,18 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/redalert233/iptv-resource/refs/heads/main/xyyd.txt"
+    "https://raw.githubusercontent.com/redalert233/iptv-resource/refs/heads/main/xyyd.txt",
+    "https://gitee.com/hw2837/iptv/raw/master/iptv.txt",
+    "https://raw.githubusercontent.com/nuzmz/live-TV/refs/heads/main/tv.txt",
+    "https://github.com/jjjay001/sy-iptv/blob/main/ShaanxiTV.m3u",
+    "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u"
 ]
 
 url_blacklist = [
     "epg.pw/stream/",
-    "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
-    "8.210.140.75:68",
-    "154.12.50.54",
-    "8.137.59.151",
-    "histar.zapi.us.kg",
-    "www.tfiplaytv.vip",
-    "111.230.30.193",
-    "148.135.93.213:81",
-    "live.goodiptv.club",
-    "iptv.luas.edu.cn"
+    "freetv",
+    "migu.188766.xyz",
+    "iptv.catvod.com"
 ]
 
 announcements = [
